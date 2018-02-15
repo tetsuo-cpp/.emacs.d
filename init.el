a2f9738a17b8f4781e05dcaf9e5593d5bbd844df
Alex Cameron <ascottcameron@gmail.com>
2018-02-15 21:52:17 +1100

    Move Avy bindings to Use Package.

--- init.el
+++ init.el
@@ -110,9 +110,8 @@
 
 ;; Jump to visible text.
 (use-package avy
-  :init
-  (global-set-key (kbd "C-;") 'avy-goto-line)
-  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  :bind (("C-;" . 'avy-goto-line)
+         ("C-'" . 'avy-goto-char-2))
   :config
   (avy-setup-default))
 
