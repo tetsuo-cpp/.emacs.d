b031c9d5e5db5504c17012bc4aa0838318e4d29c
Alex Cameron <ascottcameron@gmail.com>
2018-03-03 14:24:57 +1100

    Less hackery.

--- global/init_global.sh
+++ global/init_global.sh
@@ -3,14 +3,10 @@
 # Build container if it doesn't exist already.
 docker build -t global_env .
 
-# Symlink docker host to synchronize path with container.
-# Nasty hack... is there a better way to do this?
-sudo ln -fs ${PWD} /global_mount
-
 # Keep container running in the background.
 # We can now redirect global/gtags calls to this running container with 'docker exec'.
 docker run -d --rm \
-       -v ${PWD}:/global_mount:z \
+       -v ${PWD}:/${PWD}:z \
        --name global_env \
        -t global_env \
        bash
